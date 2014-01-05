79a467056a0ac43d8724a49d543f3c2dbe7372b3
leprasmurf <leprasmurf@gmail.com>
2014-01-05 06:01:27 +0000

    needed to grab the image and flavor objects associated with the user's selection. Limited number of instances that could be created to 3, as per the challenge. Script will now build the instances, still need to inject the SSH key

--- challenge2.php
+++ challenge2.php
@@ -15,15 +15,45 @@ $i = 0;
 $location_array = array("DFW", "ORD", "IAD", "LON", "HKG", "SYD");
 $build_location = 0;
 $image_array = array();
-$build_image = -1;
+$image_selection = -1;
+$build_image = null;
 $flavor_array = array();
-$build_flavor = 0;
+$flavor_selection = 0;
+$build_flavor = null;
 $number_of_instances = 1;
-$max_instances = 1000;
+$max_instances = 3;
 $basename = null;
 $valid = false;
-
 $handle = fopen("php://stdin", "r");
+
+function get_numeric_input($upper_limit) {
+	$valid = false;
+	$handle = fopen("php://stdin", "r");
+
+	while(!$valid) {
+		$line = fgets($handle);
+		$input = intval(trim($line));
+
+		if(($input >= 0) && ($input < $upper_limit)) {
+			$valid = true;
+		} else {
+			printf("Invalid selection: %s\n", trim($line));
+		}
+	}
+
+	return $input;
+}
+
+function print_list($array_to_display, $default) {
+	foreach($array_to_display as $key => $item) {
+		if($key == $default) {
+			// Ansi escape sequence to underline the default
+			printf("\033[4;37;40m");
+		}
+		// Ansi escape sequence to reset any previous formatting
+		printf("%d - %s\033[0;;m\n", $key, $item);
+	}
+}
 
 // Setup the client with the appropriate credentials
 $client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array (
@@ -34,25 +64,11 @@ $client = new Rackspace(Rackspace::US_IDENTITY_ENDPOINT, array (
 // Request user input for the datacenter to buid in
 // print out a list of datacenters
 printf("Which Datacenter would you like to build the server in?\n");
-foreach ($location_array as $key => $location) {
-	if($key == $build_location) {
-		printf("\033[4;37;40m");
-	}
-	printf("%d - %s\033[0;;m\n", $key, $location);
-}
+print_list($location_array, $build_location);
 
-while(!$valid) {
-	$line = fgets($handle);
-	$input = intval(trim($line));
+$build_location = get_numeric_input(count($location_array));
 
-	if(($input >= 0) && ($input < count($location_array))) {
-		$build_location = $input;
-//		printf("You selected to build the instance in %s\n", $location_array[$build_location]);
-		$valid = true;
-	} else {
-		printf("Invalid selection: %s\n", trim($line));
-	}
-}
+printf("Retrieving list of Images...\n");
 
 // Create the compute object, specify the datacenter to work within
 $compute = $client->computeService('cloudServersOpenStack', $location_array[$build_location]);
@@ -66,31 +82,21 @@ while($image = $images->next()) {
 }
 
 // Request user input for the Image to buid
-// print out a list of available images
 printf("Which Image would you like to use to build the server?\n");
-foreach ($image_array as $key => $image) {
-	if($key == $build_image) {
-		printf("\033[4;37;40m");
-	}
-	printf("%d - %s\033[0;;m\n", $key, $image);
-}
+// print out a list of available images
+print_list($image_array, $image_selection);
+// Read user input and assign selection to variable
+$image_selection = get_numeric_input(count($image_array));
 
-$valid = false;
-while(!$valid) {
-	$line = fgets($handle);
-	$input = intval(trim($line));
-
-	if(($input >= 0) && ($input < count($image_array))) {
-		$build_image = $input;
-//		printf("You selected to build the %s image.\n", $image_array[$build_image]);
-		$valid = true;
-	} else {
-		printf("Invalid selection: %s\n", trim($line));
+// Cycle through the image array
+foreach($images as $image) {
+	if(strpos($image->name, $image_array[$image_selection]) !== false) {
+		$build_image = $image;
+		break;
 	}
 }
 
-//echo gettype($images);
-print_r($images);
+printf("Retrieving list of Flavors...\n");
 
 // Grab a list of available flavors of compute instances to build
 $flavors = $compute->flavorList();
@@ -103,41 +109,18 @@ while($flavor = $flavors->next()) {
 // Request user input for the Flavor to buid
 // print out a list of available flavors
 printf("Which Flavor would you like to build?\n");
-foreach($flavor_array as $key => $flavor) {
-	if($key == $build_flavor) {
-		printf("\033[4;37;40m");
-	}
-	printf("%d - %s\033[0;;m\n", $key, $flavor);
-}
+print_list($flavor_array, $flavor_selection);
 
-$valid = false;
-while(!$valid) {
-	$line = fgets($handle);
-	$input = intval(trim($line));
+$flavor_selection = get_numeric_input(count($flavor_array));
 
-	if(($input >= 0) && ($input < count($flavor_array))) {
-		$build_flavor = $input;
-//		printf("You selected to build a %s server.\n", $flavor_array[$build_flavor]);
-		$valid = true;
-	} else {
-		printf("Invalid selection: %s\n", trim($line));
+foreach($flavors as $flavor) {
+	if(strpos($flavor->name, $flavor_array[$flavor_selection]) !== false) {
+		$build_flavor = $flavor;
+		break;
 	}
 }
-
-printf("How many servers would you like to create?\n");
-$valid = false;
-while(!$valid) {
-	$line = fgets($handle);
-	$input = intval(trim($line));
-	
-	if(($input >= 0) && ($input < $max_instances)) {
-		$number_of_instances = $input;
-//		printf("You selected to build %d servers.\n", $number_of_instances);
-		$valid = true;
-	} else {
-		printf("Invalid selection: %s\n", trim($line));
-	}
-}
+printf("How many servers would you like to create? (0 - 3)\n");
+$number_of_instances = get_numeric_input($max_instances+1);
 
 printf("Please enter a base name for your new servers.\n");
 $valid = false;
@@ -154,7 +137,7 @@ while(!$valid) {
 	}
 }
 
-printf("You have indicated that you want to build %d %s instances using the %s image in %s with a basename of %s.\n", $number_of_instances, $flavor_array[$build_flavor], $image_array[$build_image], $location_array[$build_location], $basename);
+printf("You have indicated that you want to build %d %s instances using the %s image in %s with a basename of %s.\n", $number_of_instances, $flavor_array[$flavor_selection], $image_array[$image_selection], $location_array[$build_location], $basename);
 
 printf("Is this correct?\n(y/n) ");
 $valid = false;
@@ -165,7 +148,7 @@ while(!$valid) {
 	if($input == "y") {
 		$valid = true;
 	} else if($input == "n") {
-		printf("Aborting!");
+		printf("Aborting!\n");
 		exit;
 	} else {
 		printf("Invalid entry: %s\n", trim($line));
@@ -175,21 +158,21 @@ while(!$valid) {
 // Instantiate the compute server class
 $server = $compute->server();
 
-for($i=0; $i < $number_of_instances; $i++) {
+for($i=1; $i <= $number_of_instances; $i++) {
 	try {
 		$tmp_name = "$basename$i";
 
 		// Create the compute instance
 		$response = $server->create(array(
 			'name'		=> $tmp_name,
-			'image'		=> $images[$build_image],
-			'flavor'	=> $flavors[$build_flavor],
+			'image'		=> $build_image,
+			'flavor'	=> $build_flavor,
 			'networks'	=> array(
 				$compute->network(Network::RAX_PUBLIC),
 				$compute->network(Network::RAX_PRIVATE)
 			)
 		));;
-		printf("The request to build %s%d has been submitted.  The root password is %s", $basename, $i, $server->adminPass);
+		printf("The request to build %s%d has been submitted.  The root password is %s\n", $basename, $i, $server->adminPass);
 	} catch (\Guzzle\Http\Exception\BadResponseException $e) {
 		$responseBody	= (string) $e->getResponse()->getBody();
 		$statusCode	= $e->getResponse()->getStatusCode();
